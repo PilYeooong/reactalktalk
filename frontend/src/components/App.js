03fc78985c188d0654e492e3db21c9533f3e544b
PilYeong <pilyeooong@gmail.com>
2020-11-07 14:56:57 +0900

    ADD: socket.io-client 추가 및 chat 컴포넌트에서의 socket 연결 확인

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+import Chat from './Chat';
 
 function App() {
+  useEffect(() => {
+    const result = axios
+      .get('http://localhost:4000/api')
+      .then((res) => console.log(res.data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div className="App">
-      hello
+      <Chat />
     </div>
   );
 }
