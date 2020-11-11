38c2c2154f76923959d5f2bb293ef85bc0156dc8
PilYeong <pilyeooong@gmail.com>
2020-11-11 14:39:08 +0900

    ADD: initial 값 받을수있도록 수정

--- frontend/src/hooks/useInput.js
+++ frontend/src/hooks/useInput.js
@@ -1,10 +1,10 @@
 import { useState, useCallback } from 'react';
 
-export default () => {
-  const [value, setValue] = useState('');
+export default (init = '') => {
+  const [value, setValue] = useState(init);
   const handler = useCallback((e) => {
     setValue(e.target.value);
   }, []);
-  
+
   return [value, setValue, handler];
 }
